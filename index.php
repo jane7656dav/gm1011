0781503758d7a0562d8dc3089c52e8eef5bb0652
jane7656dav <57894516+jane7656dav@users.noreply.github.com>
2019-12-02 16:00:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.mcafee.com/consumer/en-us/store/m0/index.html");
+header("Location: https://www.mcafee.com/consumer/en-gb/store/m0/catalog/mtp_521/mcafee-total-protection-trial.html");
 die();
 ?>
